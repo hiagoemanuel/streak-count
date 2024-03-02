3cc093a285a37fb66530965b11e7313212fc23da
hiagoemanuel <hiagoemanuel321@hotmail.com>
2024-03-02 17:48:57 -0300

    feat: added theme colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
+    colors: {
+      dark: {
+        100: '#1E1E1E',
+        200: '#151515'
+      },
+      light: {
+        100: '#F3F3F3',
+        200: '#C2C2C2'
+      },
+      orange: '#FC6011',
+      error: '#FF4141'
+    },
     extend: {}
   },
   plugins: []
