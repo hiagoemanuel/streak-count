d219edaadb1e868bce615ef05d719bbf1024d261
hiagoemanuel <hiagoemanuel321@hotmail.com>
2024-03-05 14:49:48 -0300

    style: stylized signup page

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,32 +1,60 @@
 import Link from 'next/link'
-import * as S from '../style'
 
 export default function Signup() {
   return (
-    <S.Container>
-      <S.AuthForm action="/">
-        <S.Title>
+    <div className="h-full flex justify-center items-center">
+      <form
+        className="w-80 py-12 px-7 rounded-3xl bg-light-200 dark:bg-dark-200"
+        action="/"
+      >
+        <div className="text-center mb-1">
           <h1>Sign Up</h1>
-          <S.TextError $err={true}>
-            <p>this email is already in use,</p>
-            <p>try again</p>
-          </S.TextError>
-        </S.Title>
-        <S.Credentials>
-          <input type="text" id="email" placeholder="email" />
-          <S.PassSettings>
-            <input type="password" id="password" placeholder="password" />
-            <label htmlFor="forget-me">
-              <input type="checkbox" id="forget-me" />
-              <span>dont&#39;t forget me</span>
+          <div>
+            <p className="text-xs text-error">this email is already in use,</p>
+            <p className="text-xs text-error">try again</p>
+          </div>
+        </div>
+        <div className="mb-6 flex flex-col gap-5">
+          <input
+            className="text-input-form w-full py-3 px-2"
+            type="text"
+            name="name"
+            placeholder="name"
+          />
+          <div className="flex justify-between flex-wrap gap-1">
+            <input
+              className="text-input-form w-full py-3 px-2"
+              type="password"
+              name="password"
+              placeholder="password"
+            />
+            <label
+              className="w-fit flex items-center gap-1 text-orange text-xs hover:underline"
+              htmlFor="forget-me"
+            >
+              <input
+                className="w-3 h-3 border-2 border-solid border-orange rounded-sm appearance-none outline-none transition-colors relative hover:cursor-pointer checked:bg-orange checked:before:content-[''] checked:before:absolute checked:before:bottom-[1px] checked:before:left-[1px] checked:before:w-1.5 checked:before:h-2 checked:before:border-2 checked:before:border-solid checked:before:border-light-200 dark:checked:before:border-dark-200 checked:before:border-t-0 checked:before:border-l-0 checked:before:rotate-45"
+                type="checkbox"
+                id="forget-me"
+              />
+              <span className="cursor-pointer">dont&#39;t forget me</span>
             </label>
-            <Link href="/login">I have an account!</Link>
-          </S.PassSettings>
-        </S.Credentials>
-        <S.AuthSubmit>
-          <input type="submit" value="Sign Up" />
-        </S.AuthSubmit>
-      </S.AuthForm>
-    </S.Container>
+            <Link
+              className="text-orange text-xs hover:underline cursor-pointer"
+              href="/login"
+            >
+              I have an account!
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <input
+            className="w-full p-2 rounded-2xl cursor-pointer text-xl bg-orange"
+            type="submit"
+            value="Sign Up"
+          />
+        </div>
+      </form>
+    </div>
   )
 }
