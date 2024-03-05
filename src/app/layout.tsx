747e86be666cb8177c8b2b9fc45f65f61e63ea88
hiagoemanuel <hiagoemanuel321@hotmail.com>
2024-03-05 14:56:32 -0300

    docs: uninstall styled-components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,10 @@ import { Aldrich } from 'next/font/google'
 import type { Metadata } from 'next'
 import { cookies } from 'next/headers'
 
-import ThemeProvider from '@/contexts/theme'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
-import '@/styles/global.css'
+import './global.css'
 
 const aldrich = Aldrich({ weight: ['400'], subsets: ['latin'] })
 
@@ -22,20 +21,18 @@ export default function RootLayout(props: {
   const themeCookie = cookies().get('USER_THEME')
 
   return (
-    <ThemeProvider>
-      <html
-        className={`${themeCookie?.value === 'dark' ? 'dark' : ''} bg-light-100 text-dark-100 dark:bg-dark-100 dark:text-light-100`}
-        lang="en"
-      >
-        <body className={aldrich.className}>
-          <main className="h-svh p-7 flex flex-col justify-between">
-            <Header />
-            {props.children}
-            <Footer />
-          </main>
-          {props.modal}
-        </body>
-      </html>
-    </ThemeProvider>
+    <html
+      className={`${themeCookie?.value === 'dark' ? 'dark' : ''} bg-light-100 text-dark-100 dark:bg-dark-100 dark:text-light-100`}
+      lang="en"
+    >
+      <body className={aldrich.className}>
+        <main className="h-svh p-7 flex flex-col justify-between">
+          <Header />
+          {props.children}
+          <Footer />
+        </main>
+        {props.modal}
+      </body>
+    </html>
   )
 }
