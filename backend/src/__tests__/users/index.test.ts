afd4c75a0fd33d35343f1d8d5732650e09db7c2f
hiagoemanuel <hiagoemanuel321@hotmail.com>
2024-03-23 20:01:45 -0300

    test: should update a user

--- backend/src/__tests__/users/index.test.ts
+++ backend/src/__tests__/users/index.test.ts
@@ -1,8 +1,10 @@
 import { HttpStatus } from '../../controllers/protocols'
 import { CreateUserController } from '../../controllers/users/create'
+import { UpdateUserController } from '../../controllers/users/update'
 import { MongoClient } from '../../database/mongodb'
 import { MongoCreateUserRepository } from '../../repositories/users/create/mongodb'
-import { CreateUserParamsType } from '../../schemas/user'
+import { MongoUpdateUserRepository } from '../../repositories/users/update/mongodb'
+import { CreateUserParamsType, UpdateUserParamsType } from '../../schemas/user'
 
 describe('User', () => {
   beforeAll(async () => await MongoClient.connect('isTest'))
@@ -11,18 +13,46 @@ describe('User', () => {
     await MongoClient.client.close()
   })
 
-  it('should create an user', async () => {
-    const req: CreateUserParamsType = {
-      name: 'testing',
-      email: 'test@email.com',
-      password: 'testing123'
+  it('should create a user', async () => {
+    const userParams: CreateUserParamsType = {
+      name: 'create user',
+      email: 'createuser@email.com',
+      password: 'createuser123'
     }
 
     const createUserRepository = new MongoCreateUserRepository()
     const createUserController = new CreateUserController(createUserRepository)
-    const response = await createUserController.handler({ body: req })
+    const response = await createUserController.handler({ body: userParams })
 
     expect(response.statusCode).toBe(HttpStatus.CREATED)
-    expect(response.body?.name).toEqual('testing')
+    expect(response.body?.name).toEqual('create user')
+  })
+
+  it('should update a user', async () => {
+    const createUserParams: CreateUserParamsType = {
+      name: 'update user',
+      email: 'updateuser@email.com',
+      password: 'updateuser123'
+    }
+
+    const updateUserParams: UpdateUserParamsType = {
+      name: 'updated',
+      email: 'updated@email.com',
+      password: 'updated123'
+    }
+
+    const createUserRepository = new MongoCreateUserRepository()
+    const createUserController = new CreateUserController(createUserRepository)
+    const userCreated = await createUserController.handler({ body: createUserParams })
+
+    const updateUserRepository = new MongoUpdateUserRepository()
+    const updateUserController = new UpdateUserController(updateUserRepository)
+    const response = await updateUserController.handler({
+      body: updateUserParams,
+      params: { id: userCreated.body?.id ?? '' }
+    })
+
+    expect(response.statusCode).toBe(HttpStatus.OK)
+    expect(response.body?.name).toEqual('updated')
   })
 })
