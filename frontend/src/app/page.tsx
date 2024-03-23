4225ca5b34072c9029f85f48026f89eb358a83d3
hiagoemanuel <hiagoemanuel321@hotmail.com>
2024-03-23 10:00:38 -0300

    fix: vercel config for deploy

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,11 @@
 import { api } from '@/lib/axios'
 import Link from 'next/link'
 
-const streakListTest = [
-  { title: 'kekw', counter: 777 },
-  { title: 'Pog', counter: 509 },
-  { title: 'awrqw', counter: 5555 },
-  { title: 'adadwaeaw', counter: 3123 },
-  { title: 'dasdaw', counter: 112 },
-  { title: 'adsa', counter: 12 },
-  { title: 'faweqweqwe', counter: 7 }
-]
-
 export default async function Home() {
   const user = await api.get('/users/65fb6ffc454aa43562e5bc8d')
 
-  const streaks = user.data.body.streakCounts
+  const streaks: Array<{ name: string; count: number }> =
+    user.data.body.streakCounts
 
   return (
     <div className="w-10/12 self-center xl:w-216">
