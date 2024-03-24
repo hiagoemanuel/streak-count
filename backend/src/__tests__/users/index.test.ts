7ebcb408fc34d1653a11af94cffb38cc733436f6
hiagoemanuel <hiagoemanuel321@hotmail.com>
2024-03-24 15:03:05 -0300

    test: should delete a user

--- backend/src/__tests__/users/index.test.ts
+++ backend/src/__tests__/users/index.test.ts
@@ -1,8 +1,10 @@
 import { HttpStatus } from '../../controllers/protocols'
 import { CreateUserController } from '../../controllers/users/create'
+import { DeleteUserController } from '../../controllers/users/delete'
 import { UpdateUserController } from '../../controllers/users/update'
 import { MongoClient } from '../../database/mongodb'
 import { MongoCreateUserRepository } from '../../repositories/users/create/mongodb'
+import { MongoDeleteUserRepository } from '../../repositories/users/delete/mongodb'
 import { MongoUpdateUserRepository } from '../../repositories/users/update/mongodb'
 import { CreateUserParamsType, UpdateUserParamsType } from '../../schemas/user'
 
@@ -14,7 +16,7 @@ describe('User', () => {
   })
 
   it('should create a user', async () => {
-    const userParams: CreateUserParamsType = {
+    const createUserParams: CreateUserParamsType = {
       name: 'create user',
       email: 'createuser@email.com',
       password: 'createuser123'
@@ -22,7 +24,7 @@ describe('User', () => {
 
     const createUserRepository = new MongoCreateUserRepository()
     const createUserController = new CreateUserController(createUserRepository)
-    const response = await createUserController.handler({ body: userParams })
+    const response = await createUserController.handler({ body: createUserParams })
 
     expect(response.statusCode).toBe(HttpStatus.CREATED)
     expect(response.body?.name).toEqual('create user')
@@ -54,5 +56,24 @@ describe('User', () => {
 
     expect(response.statusCode).toBe(HttpStatus.OK)
     expect(response.body?.name).toEqual('updated')
+  })
+
+  it('should delete a user', async () => {
+    const createUserParams: CreateUserParamsType = {
+      name: 'delete',
+      email: 'delete@email.com',
+      password: 'delete123'
+    }
+
+    const createUserRepository = new MongoCreateUserRepository()
+    const createUserController = new CreateUserController(createUserRepository)
+    const userCreated = await createUserController.handler({ body: createUserParams })
+
+    const deleteUserRepository = new MongoDeleteUserRepository()
+    const deleteUserController = new DeleteUserController(deleteUserRepository)
+    const response = await deleteUserController.handler({ params: { id: userCreated.body?.id ?? ''} })
+
+    expect(response.statusCode).toBe(HttpStatus.OK)
+    expect(response.body?.name).toEqual('delete')
   })
 })
